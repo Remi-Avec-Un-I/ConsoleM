f778b700232aee9f2929333bec9562a5fedd0ad8
Remi Avocat <remi.avocat@etu.umontpellier.fr>
2025-03-19 01:12:51 +0100

    Improve documentation styling

--- ConsoleM/docs/conf.py
+++ ConsoleM/docs/conf.py
@@ -16,7 +16,15 @@ release = '0.1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -26,5 +34,25 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# Theme options
+html_theme_options = {
+    'navigation_depth': 4,
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'includehidden': True,
+    'titles_only': False,
+    'logo_only': False,
+    'style_nav_header_background': '#2980B9',
+    'style_external_links': True,
+    'style_admonition_text_color': '#2980B9',
+    'style_admonition_title_color': '#2980B9',
+    'style_admonition_background_color': '#F8F9FA',
+    'style_admonition_border_color': '#2980B9',
+}
+
+# Custom CSS
+def setup(app):
+    app.add_css_file('custom.css')
