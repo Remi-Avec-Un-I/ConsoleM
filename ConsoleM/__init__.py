0d7e75b2949efe32600488608e1d655d9b6768fb
Remi Avocat <remi.avocat@etu.umontpellier.fr>
2025-03-20 00:34:00 +0100

    try again [patch]

--- ConsoleM/__init__.py
+++ ConsoleM/__init__.py
@@ -3,5 +3,5 @@
 from .Style import Text
 from .Core import Terminal, Keys
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __all__ = ["Text", "Terminal", "Keys"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="ConsoleM",
-    version="0.1.2",
+    version="0.1.3",
     author="Remi",
     description="A powerful Python library for terminal manipulation and text styling",
     long_description=long_description,
